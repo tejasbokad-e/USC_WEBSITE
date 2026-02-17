dace6544c15b2aaac5c3c29183b4d5173ceb43f3
miaoda <miaoda@baidu.com>
2026-02-17 18:55:07 +0530

    clean

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Option {
-  label: string;
-  value: string;
-  icon?: React.ComponentType<{ className?: string }>;
-  withCount?: boolean;
-}
-
-// Re-export all types from types.ts
-export * from './types';
